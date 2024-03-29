a01230518849a17ce0378bbe0a906e74aecb8bce
CJG <cjgastan@gmail.com>
2024-03-29 13:19:03 -0400

    -

--- algorithms/Cube.py
+++ algorithms/Cube.py
@@ -1,102 +1,227 @@
 import numpy as np
+import random
 import copy
 
+#f/b - z
+#r/l - x
+#u/d - y
+
 class Cube:
-    def __init__(self) -> None:
-        self.sides = np.array([[['w', 'w', 'w'],['w', 'w', 'w'],['w','w','w']], [['r', 'r', 'r'],['r', 'r', 'r'],['r','r','r']], [['b', 'b', 'b'],['b', 'b', 'b'],['b','b','b']],
-                      [['o', 'o', 'o'],['o', 'o', 'o'],['o','o','o']], [['g', 'g', 'g'],['g', 'g', 'g'],['g','g','g']], [['y', 'y', 'y'],['y', 'y', 'y'],['y','y','y']]])
-        self.scramble = []
+    def __init__(self, state = None) -> None:
+        self.colors = ['w', 'g', 'r', 'b', 'o', 'y']
+        if state == None:
+            self.unscrambled()
+            self.moves = "rlfbduRLFBUD"
+        else:
+            self.cube = [[[]]]
+            for i, s in enumerate(state):
+                self.cube[-1][-1].append(s)
+                if len(self.cube[-1][-1]) == 3 and len(self.cube[-1]) < 3:
+                    self.cube[-1].append([])
+                elif len(self.cube[-1][-1]) == 3 and len(self.cube[-1]) == 3 and i < len(state) - 1:
+                    self.cube.append([[]])
 
     def input(self, sides):
-        self.sides = sides
-    
+        self.cube = sides
+
+    def unscrambled(self):
+        self.cube = [[[s for x in range(3)] for y in range(3)] for s in self.colors]
+        self.scramble = ""
+
     def printCube(self):
         for i in range(3):
-            print(self.sides[0][i][:], self.sides[1][i][:], self.sides[2][i][:], self.sides[3][i][:], self.sides[4][i][:], self.sides[5][i][:])
+            print(self.cube[0][i][:], self.cube[1][i][:], self.cube[2][i][:], self.cube[3][i][:], self.cube[4][i][:], self.cube[5][i][:])
+
+    def stringify(self):
+        return ''.join([i for r in self.cube for s in r for i in s])
 
     def turnCube(self, turns):
         self.scramble = turns
         for turn in turns:
-            letter = turn[0].lower()
-            num = int(turn[1])
-            print(letter, num)
-            match letter:
+            match turn:
                 case 'r':
-                    self.turnRight(num)
+                    self.xRotation(2, 0)
                 case 'l':
-                    self.turnLeft(num)
+                    self.xRotation(0, 1)
                 case 'f':
-                    self.turnFront(num)
+                    self.zRotation(2, 0)
                 case 'b':
-                    self.turnBack(num)
+                    self.zRotation(0, 1)
                 case 'd':
-                    self.turnDown(num)
+                    self.yRotation(2, 0)
                 case 'u':
-                    self.turnUp(num)
+                    self.yRotation(0, 1)
+                case 'R':
+                    self.xRotation(2, 1)
+                case 'L':
+                    self.xRotation(0, 0)
+                case 'F':
+                    self.zRotation(2, 1)
+                case 'B':
+                    self.zRotation(0, 0)
+                case 'D':
+                    self.yRotation(2, 1)
+                case 'U':
+                    self.yRotation(0, 0)
+            print(turn)
             self.printCube()
             
     #5,i,2 -> 3,2-i,0
     #3,2-i,2 -> 5,i,0
 
-    def turnRight(self, num):
-        self.sides[4] = np.rot90(self.sides[4], 4-int(num))
-        for x in range(int(num)):
-            sides = copy.deepcopy(self.sides)
-            for i in range(3):
-                self.sides[1][i][2] = sides[0][i][2]
-                self.sides[5][i][2] = sides[1][i][2]
-                self.sides[3][2-i][0] = sides[5][i][2]
-                self.sides[0][i][2] = sides[3][2-i][0]
+    #U or D
+    def yRotation(self, row, dir):
+        cube = copy.deepcopy(self.cube)
+        if dir == 0:
+            self.cube[1][row], self.cube[2][row], self.cube[3][row], self.cube[4][row] = (cube[2][row],
+                                                                                        cube[3][row],
+                                                                                        cube[4][row],
+                                                                                        cube[1][row])
+            if row == 0:
+                #self.cube[0] = np.rot90(self.cube[0], axes=(1,0))
+                self.cube[0] = [list(x) for x in zip(*reversed(cube[0]))]
 
-    def turnLeft(self, num):
-        self.sides[2] = np.rot90(self.sides[2], 4-int(num))
-        for x in range(int(num)):
-            sides = copy.deepcopy(self.sides)
-            for i in range(3):
-                self.sides[1][i][0] = sides[5][i][0]
-                self.sides[0][i][0] = sides[1][i][0]
-                self.sides[3][2-i][2] = sides[0][i][0]
-                self.sides[5][i][0] = sides[3][2-i][2]
+            elif row == 2:
+                #self.cube[5] = np.rot90(self.cube[5], axes=(0,1))
+                self.cube[5] = [list(x) for x in zip(*cube[5])][::-1]
 
-    #
+        else: #Twist right
+            self.cube[1][row], self.cube[2][row], self.cube[3][row], self.cube[4][row] = (cube[4][row],
+                                                                                            cube[1][row],
+                                                                                            cube[2][row],
+                                                                                            cube[3][row])
+            if row == 0:
+                #self.cube[0] = np.rot90(self.cube[0], axes=(0,1))
+                self.cube[0] = [list(x) for x in zip(*cube[0])][::-1]
+            elif row == 2:
+                #self.cube[5] = np.rot90(self.cube[5], axes=(1,0))
+                self.cube[5] = [list(x) for x in zip(*reversed(cube[5]))]        
 
-    def turnFront(self, num):
-        self.sides[1] = np.rot90(self.sides[1], 4-int(num))
-        for x in range(int(num)):
-            sides = copy.deepcopy(self.sides)
-            for i in range(3):
-                self.sides[2][i][2] = sides[0][0][i]
-                self.sides[5][2][2-i] = sides[2][i][2]
-                self.sides[4][i][0] = sides[5][2][i]
-                self.sides[0][0][2-i] = sides[4][i][0]
+    #R or L
+    def xRotation(self, col, dir):
+        cube = copy.deepcopy(self.cube)
+        for i in range(3):
+            if dir == 0:
+                self.cube[0][i][col], self.cube[2][i][col], self.cube[4][-i-1][-col-1], self.cube[5][i][col] = (cube[4][-i-1][-col-1],
+                                                                                                                cube[0][i][col],
+                                                                                                                cube[5][i][col],
+                                                                                                                cube[2][i][col])
+            else: #Twist right
+                self.cube[0][i][col], self.cube[2][i][col], self.cube[4][-i-1][-col-1], self.cube[5][i][col] = (cube[2][i][col],
+                                                                                                                cube[5][i][col],
+                                                                                                                cube[0][i][col],
+                                                                                                                cube[4][-i-1][-col-1])
+                
+        if dir == 0:
+            if col == 0:
+                #self.cube[1] = np.rot90(self.cube[1], axes=(1,0))
+                self.cube[1] = [list(x) for x in zip(*reversed(cube[1]))]
+            elif col == 2:
+                #self.cube[3] = np.rot90(self.cube[3], axes=(0,1))
+                self.cube[3] = [list(x) for x in zip(*cube[3])][::-1] 
+        else:
+            if col == 0:
+                #self.cube[1] = np.rot90(self.cube[1], axes=(0,1))
+                self.cube[1] = [list(x) for x in zip(*cube[1])][::-1]
+            elif col == 2:
+                #self.cube[3] = np.rot90(self.cube[3], axes=(1,0))
+                self.cube[3] = [list(x) for x in zip(*reversed(cube[3]))]             
 
-    def turnBack(self, num):
-        self.sides[3] = np.rot90(self.sides[3], 4-int(num))
-        for x in range(int(num)):
-            sides = copy.deepcopy(self.sides)
-            for i in range(3):
-                self.sides[4][2-i][2] = sides[0][2][i]
-                self.sides[5][0][i] = sides[4][i][2]
-                self.sides[2][2-i][0] = sides[5][0][i]
-                self.sides[0][2][i] = sides[2][i][0]
-
-    def turnDown(self, num):
-        self.sides[0] = np.rot90(self.sides[0], 4-int(num))
-        for x in range(int(num)):
-            sides = copy.deepcopy(self.sides)
-            self.sides[4][2] = sides[1][2]
-            self.sides[3][2] = sides[4][2]
-            self.sides[2][2] = sides[3][2]
-            self.sides[1][2] = sides[2][2]
-
-    def turnUp(self, num):
-        self.sides[5] = np.rot90(self.sides[5], 4-int(num))
-        for x in range(int(num)):
-            sides = copy.deepcopy(self.sides)
-            self.sides[2][0] = sides[1][0]
-            self.sides[3][0] = sides[2][0]
-            self.sides[4][0] = sides[3][0]
-            self.sides[1][0] = sides[4][0]
+    #F or B
+    def zRotation(self, col, dir):
+        cube = copy.deepcopy(self.cube)
+        for i in range(3):
+            if dir == 0:
+                self.cube[0][col][i], self.cube[1][-i-1][col], self.cube[3][i][-col-1], self.cube[5][-col-1][-1-i] = (cube[3][i][-col-1],
+                                                                                                                    cube[0][col][i],
+                                                                                                                    cube[5][-col-1][-1-i],
+                                                                                                                    cube[1][-i-1][col])
+            else: #Twist right
+                self.cube[0][col][i], self.cube[1][-i-1][col], self.cube[3][i][-col-1], self.cube[5][-col-1][-1-i] = (cube[1][-i-1][col],
+                                                                                                                    cube[5][-col-1][-1-i],
+                                                                                                                    cube[0][col][i],
+                                                                                                                    cube[3][i][-col-1])
+                
+        if dir == 0:
+            if col == 0:
+                #self.cube[4] = np.rot90(self.cube[4], axes=(1,0))
+                self.cube[4] = [list(x) for x in zip(*reversed(cube[4]))]
+            elif col == 2:
+                #self.cube[2] = np.rot90(self.cube[2], axes=(0,1))
+                self.cube[2] = [list(x) for x in zip(*cube[2])][::-1] 
+        else:
+            if col == 0:
+                #self.cube[4] = np.rot90(self.cube[4], axes=(0,1))
+                self.cube[4] = [list(x) for x in zip(*cube[4])][::-1]
+            elif col == 2:
+                #self.cube[2] = np.rot90(self.cube[2], axes=(1,0))
+                self.cube[2] = [list(x) for x in zip(*reversed(cube[2]))] 
 
     def solve(self):
         scramble = copy.deepcopy(self.scramble)
+
+    def printMoves(self, moves):
+        solve = ""
+        for x in moves:
+            if x[0] == 'v':
+                if x[1] == 0:
+                    if x[2] == 0:
+                        solve += 'L'
+                    else:
+                        solve += 'l'
+                else:
+                    if x[2] == 0:
+                        solve += 'r'
+                    else:
+                        solve += 'R'
+            elif x[0] == 'h':
+                if x[1] == 0:
+                    if x[2] == 0:
+                        solve += 'U'
+                    else:
+                        solve += 'u'
+                else:
+                    if x[2] == 0:
+                        solve += 'd'
+                    else:
+                        solve += 'D'
+            else:
+                if x[1] == 0:
+                    if x[2] == 0:
+                        solve += 'B'
+                    else:
+                        solve += 'b'
+                else:
+                    if x[2] == 0:
+                        solve += 'f'
+                    else:
+                        solve += 'F'
+        print(solve)
+        
+
+    def randomScramble(self, moveCount):
+        scramble = ""
+        for x in range(moveCount):
+            scramble += random.choice(self.moves)
+        print(scramble)
+        self.turnCube(scramble)
+
+    def reverseMoves(self, moves):
+        return moves.swapcase()[::-1]
+    
+    def solved(self):
+        for side in self.cube:
+            hold = []
+            check = True
+            for row in side:
+                if len(set(row)) == 1:
+                    hold.append(row[0])
+                else:
+                    check = False
+                    break
+            if not(check):
+                break
+            if len(set(hold)) > 1:
+                check = False
+                break
+        return check
--- algorithms/main.py
+++ algorithms/main.py
@@ -1,14 +1,66 @@
-from Cube import Cube
+import json
+import os.path
+import time
 
+from Cube import Cube
+from solver import IDA_star, build_heuristic_db
+
+MAX_MOVES = 8
+NEW_HEURISTICS = False
+HEURISTIC_FILE = 'heuristic.json'
+
+#--------------------------------
 cube = Cube()
 cube.printCube()
-moves = ["r2","l3", "u1", "b1", "u2", "d1", "l1", "u3", "d1", "l2", "u2", "b1", "u1", "l2", "d2", "f3", "b3", "d3", "r2", "u1", "f1", "l2", "f1", "l1", "b2"]
-print(len(moves))
-cube.turnCube(moves)
-#cube.turnCube(["r3"])
-print("-----------------------------")
+print('-----------')
+#--------------------------------
+
+if os.path.exists(HEURISTIC_FILE):
+    with open(HEURISTIC_FILE) as f:
+        h_db = json.load(f)
+else:
+    h_db = None
+
+if h_db is None or NEW_HEURISTICS is True:
+    actions = [(r, n, d) for r in ['h', 'v', 's'] for d in [0, 1] for n in [0,2]]
+    h_db = build_heuristic_db(
+        cube.stringify(),
+        actions,
+        max_moves = MAX_MOVES,
+        heuristic = h_db
+    )
+
+    with open(HEURISTIC_FILE, 'w', encoding='utf-8') as f:
+        json.dump(
+            h_db,
+            f,
+            ensure_ascii=False,
+            indent=4
+        )
+#--------------------------------
+cube.randomScramble(12)
+#l_rot = MAX_MOVES if MAX_MOVES < 5 else 5,
+#u_rot = MAX_MOVES
+
+print('----------')
 cube.printCube()
+print('----------')
+start = time.perf_counter()
+#--------------------------------
+solver = IDA_star(h_db)
+moves = solver.run(cube.stringify())
+end = time.perf_counter()
 
+print("Moves: ")
+cube.printMoves(moves)
+print('----------')
 
-#5,i,2 -> 3,2-i,0
-#3,2-i,2 -> 5,i,0
+for m in moves:
+    if m[0] == 'h':
+        cube.yRotation(m[1], m[2])
+    elif m[0] == 'v':
+        cube.xRotation(m[1], m[2])
+    elif m[0] == 's':
+        cube.zRotation(m[1], m[2])
+cube.printCube()
+print("Time: ", end - start, "s")
